0d3ef5fff46deac03e7c2130c57a69e6cafe227d
Samuel Kelemen <SCKelemen@users.noreply.github.com>
2019-12-11 12:47:45 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/SCKelemen/modules
+
+go 1.13
+
+require (
+	exclaim v0.0.0
+	yell v0.0.0
+)
+
+replace exclaim v0.0.0 => ./exclaim
+
+replace yell v0.0.0 => ./yell
---
+++ main.go
@@ -0,0 +1,14 @@
+package main 
+
+import (
+"exclaim"
+"yell"
+	"fmt"
+)
+
+
+func main() {
+    fmt.Println("Sanity Check")
+    yell.Yell("Sanity Check")
+    exclaim.Exclaim("Sanity Check")
+}
